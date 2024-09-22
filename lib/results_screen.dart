a9410b5fcbb312a7ce245113c517599c4984300b
surazz999 <surazsub999@gmail.com>
2024-09-22 21:20:06 +0545

    final assignment

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/questions.dart';
+import 'package:flutter_application_2/questions_summary.dart';
 
 class ResultScreen extends StatelessWidget {
-  const ResultScreen({super.key});
+  const ResultScreen({super.key, required this.chosenAnswers});
+  final List<String> chosenAnswers;
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectAnswer = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -12,11 +32,14 @@ class ResultScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You answered X out of Y questions correctly!'),
+            Text(
+                'You answered $numCorrectAnswer out of  $numTotalQuestions correctly!'),
             const SizedBox(
               height: 30,
             ),
-            const Text('List of answers and questions..'),
+            QuestionsSummary(
+              getSummaryData(),
+            ),
             const SizedBox(
               height: 30,
             ),
